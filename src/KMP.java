34854b4969280dfde0f67d7f503149d4e7386ae9
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2022-11-29 02:39:31 +0200

    Fixed kmp bug

--- src/KMP.java
+++ src/KMP.java
@@ -7,6 +7,7 @@ public class KMP extends Algorithm{
         match();
     }
     public int[] prefixFunction() {
+        System.out.println("Prefix function:");
         int[] result = new int[M];
         for (int i = 0; i < M; i++) {
             String current = pattern.substring(0, i+1);
@@ -42,32 +43,30 @@ public class KMP extends Algorithm{
         int[] prefix = prefixFunction();
         int q = 0;
         System.out.println("Algorithm:");
-        String patternShifted = pattern;
         System.out.println(sequence);
         for (int i = 0; i < N; i++) {
-            boolean printed = false;
-            while (q > 0 && patternShifted.charAt(q) != sequence.charAt(i)) {
-                System.out.println(ColorCode.ANSI_YELLOW + patternShifted.charAt(q) + ColorCode.ANSI_RESET);
-                q = prefix[q];
-                shift(i);
-                printed = true;
+            char currentChar = sequence.charAt(i);
+            while (q > 0 && pattern.charAt(q) != currentChar) {
+                shift(i-q);
+                q = prefix[q - 1];
+                System.out.println(pattern);
             }
-            if (patternShifted.charAt(q) == sequence.charAt(i)) {
-                System.out.print(patternShifted.charAt(q));
+            char charAtQ = pattern.charAt(q);
+            if (charAtQ == sequence.charAt(i)) {
                 q = q + 1;
-            }
-            else {
-                if (!printed) {
-                    System.out.println(ColorCode.ANSI_RED + patternShifted.charAt(q) + ColorCode.ANSI_RESET);
-
+                if (q == M) {
+                    this.shift(i-M+1);
+                    result = i - M + 1;
+                    System.out.println(ColorCode.ANSI_GREEN + pattern + ColorCode.ANSI_RESET);
+                    break;
                 }
             }
-            if (q == M) {
-                this.shift(i);
-                System.out.println(ColorCode.ANSI_GREEN + pattern + ColorCode.ANSI_RESET);
-                result = i - M + 1;
-                break;
+            else {
+                shift(i-q);
+                System.out.println(pattern);
+                q = 0;
             }
+
         }
     }
 }
